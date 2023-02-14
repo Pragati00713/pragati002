d510557846bf3694b2bb5f7ef7843eb53039a50c
Pragati00713 <kushwahapragati713@gmail.com>
2023-02-14 00:51:47 -0800

    lucky

---
+++ area.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+
+int main()
+// lucky
+
+
+{
+    std::cout<<"welcome in c++ program";
+    std::cout<<" pr pta nhi kese hoge";
+    std::cout<<"\n bholenath apni krapa bnaye rakhna";
+    std::cout<<"\n bholenath pese ki problem kitti ho rhi apko pta h thoda sa mind manage krwa do jisse jaldi jaldi sb sikh jau";
+    std::cout<<"\n bholenath bhut hi jyda disturb hu";
+    std::cout<<"\n be mind m ek hi question h mujhse ho paega ya nhi";
+
+    return 0;
+
+}
